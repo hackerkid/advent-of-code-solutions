15e0025b41e31aa44a31f9755d29fb11e077effa
Vishnu Ks <hackerkid@vishnuks.com>
2015-12-10 17:35:32 +0530

    day-2 added

---
+++ day-2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+	int x;
+	int y;
+	int z;
+	int a[3];
+	long long area;
+	FILE *fp = fopen("input", "r+");
+
+	area = 0;
+
+	while(fscanf(fp, "%dx%dx%d", a, a + 1,a + 2) != -1) {
+		sort(a, a +3);
+ 		area += 2 * (a[0] * a[1] + a[1] * a[2] + a[2] * a[0]) + a[0] * a[1];
+ 	}
+
+ 	cout << area << endl;
+
+ }
+
