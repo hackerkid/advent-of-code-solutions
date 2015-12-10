b8d801abcc641f8f18f8de62e15b9b4f08fef159
Vishnu Ks <hackerkid@vishnuks.com>
2015-12-10 19:13:55 +0530

    day 3 and day 4 done

---
+++ day-3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stdio.h>
+#include <map>
+#include <utility>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+
+	fstream fcin;
+	string inp;
+	pair <int, int> pos;
+	map < pair <int, int>, bool> mp;
+	int ans;
+
+	fcin.open("input", ios::in);
+	fcin >> inp;
+	
+	
+	pos.first = 0;
+	pos.second = 0;
+	mp[pos] = true;
+	ans = 1;
+
+	for (int i = 0; i < inp.length(); i++) {
+		switch(inp[i]) {
+			case '^':
+				pos.first--;
+				break;
+			case 'v':
+				pos.first++;
+				break;
+			case '>':
+				pos.second++;
+				break;
+			case '<':
+				pos.second--;
+				break;
+		}
+
+		if(mp.count(pos) == 0) {
+			ans++;
+			mp[pos] = true;
+		}
+
+	}
+
+	cout << ans << endl;
+
+}
+
---
+++ day-4.py
@@ -0,0 +1,23 @@
+import hashlib
+
+ok = 1
+i = 1
+inp = "ckczppom"
+
+while ok:
+	res = str(i)
+	coin = inp + res
+	hash_object = hashlib.md5(coin)
+	x = hash_object.hexdigest()
+	if(x[0:5] == "00000"):
+		print i
+		ok = 0
+	i = i + 1
+
+
+
+
+
+
+
+
